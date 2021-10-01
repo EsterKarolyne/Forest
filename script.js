d6bf3c132013f69cc206e7f3bb2d306844418eb1
unknown <esterknappverly@gmail.com>
2021-10-01 13:21:13 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+function birds(){
+    var passaros = document.getElementById("birdsFunc")
+    var container = document.getElementById("div-birds")
+    container.classList.toggle("birds-toglle")
+}
+
+
+
+function morning(){
+    var manha = document.getElementById("morningFunc")
+    document.body.classList.toggle("morning-toglle");
+}
+
+
+
+function afternoon(){
+    var tarde = document.getElementById("afternoonFunc")
+    document.body.classList.toggle("afternoon-toglle");
+}
+
+
+
+function night(){
+    var noite = document.getElementById("nightFunc")
+    document.body.classList.toggle("night-toglle");
+
+}
